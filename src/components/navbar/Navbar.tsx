59a1a77ba5174f2b5e07ea48e167b103649e5f3b
Lucas Oelschlager <lucasoelschlager@gmail.com>
2025-05-08 15:06:33 -0300

    se añadio animacion a los botones del nav

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import styledNav from "./Navbar.module.css";
 import { CircleHelp, Globe, CircleUserRound } from "lucide-react";
 export const Navbar = () => {
+  const [btnHovered, setBtnHovered] = useState<string | null>(null);
+
+  const handleMouseEnter = (btn: string) => {
+    setBtnHovered(btn);
+  };
+  const handleMouseLeave = () => {
+    setBtnHovered(null);
+  };
   return (
     <header className={`${styledNav.header}`}>
       <nav className={`${styledNav.navbar}`}>
@@ -9,31 +18,23 @@ export const Navbar = () => {
         </picture>
 
         <ul className={`${styledNav.list}`}>
-          <li>
-            <button className={`${styledNav.btn}`}>
-              <span>Vehicles</span>
-            </button>
-          </li>
-          <li>
-            <button className={`${styledNav.btn}`}>
-              <span>Energy</span>
-            </button>
-          </li>
-          <li>
-            <button className={`${styledNav.btn}`}>
-              <span>Charging</span>
-            </button>
-          </li>
-          <li>
-            <button className={`${styledNav.btn}`}>
-              <span>Discover</span>
-            </button>
-          </li>
-          <li>
-            <button className={`${styledNav.btn}`}>
-              <span>Shop</span>
-            </button>
-          </li>
+          {["Vehicles", "Energy", "Charging", "Discover", "Shop"].map(
+            (item) => (
+              <li key={item}>
+                <button
+                  className={
+                    btnHovered === item
+                      ? `${styledNav.btnHover}`
+                      : `${styledNav.btn}`
+                  }
+                  onMouseEnter={() => handleMouseEnter(item)}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <span>{item}</span>
+                </button>
+              </li>
+            )
+          )}
         </ul>
 
         <div className={`${styledNav.btnContainer}`}>
